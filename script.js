121292fa892bd8d70dd3f971be2f2884a1077932
GabrielAfflitto <gafflitto92@gmail.com>
2022-08-21 14:39:10 -0600

    created event listener for message form

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:3000');
 
+const messageForm = document.getElementById('send-container')
+
 socket.on('chat-message', data => {
   console.log(data);
+})
+
+messageForm.addEventListener('sumbit', e => {
+  e.preventDefault()
+  const message = messageInput.value()
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
 })
