77cbb281e0d088f333721f194d3b21973a598a4f
GabrielAfflitto <gafflitto92@gmail.com>
2022-08-21 14:34:45 -0600

    configured socket.io for cors error

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const io = require('socket.io')(3000);
+// const io = require('socket.io')(3000);
+
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', socket => {
   socket.emit('chat-message', 'hello world')
